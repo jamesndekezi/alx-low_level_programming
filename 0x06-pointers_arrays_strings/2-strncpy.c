5925151151b3cfb944e5791d8b17f81e094e9c9c
jamesndekezi <james.mick51@gmail.com>
2022-09-22 10:13:43 -0700

    yes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,14 @@
 #include "main.h"
 
 /**
-
  *_strncpy - copies a string
-
  *
-
  *@dest:destination
-
  *@src:source
-
  *@n:range
-
  *
-
  *Return:dest
-
  *
-
  */
 
 char *_strncpy(char *dest, char *src, int n)
