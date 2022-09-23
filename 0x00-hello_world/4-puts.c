1a3bc5e269eeea8f3d8dd84ab68d42379839281c
jamesndekezi <james.mick51@gmail.com>
2022-09-22 20:54:51 -0700

    yes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main- Serves as the entry point for the program
+ * Return: By default returns zero if no erros
+ */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
