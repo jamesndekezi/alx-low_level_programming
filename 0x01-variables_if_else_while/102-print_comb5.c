4384164deed1bc96fda860088fa3abc727bc6917
jamesndekezi <james.mick51@gmail.com>
2022-09-23 15:02:39 -0700

    yes

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,37 +7,45 @@
  */
 
 int main(void)
+
 {
-	int a, b, c, d;
-	for (a = '0'; a <= '9'; a++)
 
-	{
-		for (b = '0'; b <= '9'; b++)
-		{
-			for (c = '0'; c <= '9'; c++)
+int a, b, c, d;
 
-			{
-				for (d = '0'; d <= '9'; d++)
+for (a = '0'; a <= '9'; a++)
+{
 
-				{
-					if ((a < c) || (a == c && b < d))
+for (b = '0'; b <= '9'; b++)
 
-					{			
-						putchar(a);
-						putchar(b);
-						putchar(' ');
-						putchar(c);
-						putchar(d);
-					if (!(a == '9' && b == '8'))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					}
-				}
-			}
-		}
+{
 
-	putchar('\n');
-	return (0);
+for (c = '0'; c <= '9'; c++)
+
+{
+
+for (d = '0'; d <= '9'; d++)
+
+{
+
+if ((a < c) || (a == c && b < d))
+
+{
+putchar(a);
+putchar(b);
+putchar(' ');
+putchar(c);
+putchar(d);
+if (!(a == '9' && b == '8'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+
 }
